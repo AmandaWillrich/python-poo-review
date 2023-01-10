ffd3f1ffeb25ade03da202abe8d868791d653c5f
amandawillrich <amandawillrich@outlook.com>
2023-01-10 18:10:55 -0300

    version2

--- accountsettings.py
+++ accountsettings.py
@@ -1,18 +1,66 @@
-def create_account(accountnumber, owner, balance, limit):
-    account = {'accountnumber': accountnumber,
-               'owner': owner,
-               'balance': balance,
-               'limit': limit}
-    return account
+# def create_account(accountnumber, owner, balance, limit):
+#     account = {'accountnumber': accountnumber,
+#                'owner': owner,
+#                'balance': balance,
+#                'limit': limit}
+#     return account
 
 
-def deposit(account, amount):
-    account['balance'] += amount
+# def deposit(account, amount):
+#     account['balance'] += amount
 
 
-def withdraw(account, amount):
-    account['balance'] -= amount
+# def withdraw(account, amount):
+#     account['balance'] -= amount
 
 
-def show_balance(account):
-    print(f"Your current balance is R$ {account['balance']}")
+# def show_balance(account):
+#     print(f"Your current balance is R$ {account['balance']}")
+
+class Account():
+    def __init__(self, account_number, owner, balance, limit):
+        self.__account_number = account_number
+        self.__owner = owner
+        self.__balance = balance
+        self.__limit = limit
+
+    def show_balance(self):
+        print(f'The account owned by {self.__owner}',
+              f'has the balance of R$ {self.__balance}')
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def __can_withdraw(self, target_amount):
+        available_money = (self.__balance + self.__limit)
+        return target_amount <= available_money
+
+    def withdraw(self, amount):
+        if self.can_withdraw(amount):
+            self.__balance -= amount
+        else:
+            print('The amount exceeds the limit')
+
+    def transfer(self, amount, target):
+        self.withdraw(amount)
+        target.deposit(amount)
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    @property
+    def owner(self):
+        return self.__owner
+
+    @property
+    def limit(self):
+        return self.__limit
+
+    @staticmethod
+    def bank_code():
+        return '001'
+
+    @limit.setter
+    def limit(self, limit):
+        self.__limit = limit
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from accountsettings import create_account, deposit, show_balance, withdraw
+from accountsettings import Account
 
-Amandas_account = create_account(123, 'Amanda', 400, 1000)
-print(Amandas_account)
+# Amandas_account = create_account(123, 'Amanda', 400, 1000)
+# print(Amandas_account)
 
-deposit(Amandas_account, 100)
-show_balance(Amandas_account)
+# deposit(Amandas_account, 100)
+# show_balance(Amandas_account)
 
-withdraw(Amandas_account, 50)
-show_balance(Amandas_account)
+# withdraw(Amandas_account, 50)
+# show_balance(Amandas_account)
+
+account1 = Account(123, 'Amanda', 1000, 2000)
+account2 = Account(321, 'Cris', 1000, 5000)
+
+account1.transfer(200, account2)
+account2.show_balance()
+
+print(account1.balance)
+
+account1.withdraw(300)
+account1.show_balance()
+
+account1.withdraw(3000)
+account1.show_balance()
