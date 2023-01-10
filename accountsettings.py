a032e556dd061c9e446dedcee608822c5d7aefcd
amandawillrich <amandawillrich@outlook.com>
2023-01-10 14:28:09 -0300

    alt

--- accountsettings.py
+++ accountsettings.py
@@ -1,9 +1,18 @@
 def create_account(accountnumber, owner, balance, limit):
-    account = {'number': accountnumber,
+    account = {'accountnumber': accountnumber,
                'owner': owner,
                'balance': balance,
                'limit': limit}
     return account
 
-def deposit(accountnumber, amount):
-    pass
+
+def deposit(account, amount):
+    account['balance'] += amount
+
+
+def withdraw(account, amount):
+    account['balance'] -= amount
+
+
+def show_balance(account):
+    print(f"Your current balance is R$ {account['balance']}")
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from accountsettings import create_account
+from accountsettings import create_account, deposit, show_balance, withdraw
 
-create_account(123, 'Amanda', 500, 1000)
+Amandas_account = create_account(123, 'Amanda', 400, 1000)
+print(Amandas_account)
+
+deposit(Amandas_account, 100)
+show_balance(Amandas_account)
+
+withdraw(Amandas_account, 50)
+show_balance(Amandas_account)
