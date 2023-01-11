3628fe8ad217c07d512c6ed8019f2e43c9b0d031
Amanda Willrich Fuchs <111703865+AmandaWillrich@users.noreply.github.com>
2023-01-11 18:02:33 -0300

    alt

---
+++ second_part/main.py
@@ -0,0 +1,22 @@
+from model import Movies, Playlist, TvShow
+
+avengers = Movies('avengers', 2018, 180)
+this_is_us = TvShow('this is us', 2016, 7)
+
+# print(f'Nome do filme: {avengers.title} - Ano de Lançamento:',
+#       f'{avengers.release_year} - Duração: {avengers.duration}',
+#       f' - Likes: {avengers.likes}')
+
+avengers = Movies('avengers', 2018, 180)
+this_is_us = TvShow('this is us', 2016, 7)
+scream = Movies('scream', 1909, 100)
+lucifer = TvShow('lucifer', 2015, 5)
+
+# para resolver atributos diferentes em lista
+list_of_shows = [avengers, this_is_us, scream, lucifer]
+weekend_playlist = Playlist('Weekend Playlist', list_of_shows)
+
+# Neste caso, na iteração, o método é reescrito em cada classe,
+# imprimindo de maneira correta para ambos os casos (polimorfismo)
+for show in weekend_playlist.shows:
+    print(show)
---
+++ second_part/model.py
@@ -0,0 +1,47 @@
+class Entertainment:
+    def __init__(self, title, release_year):
+        self._title = title.title()
+        self.release_year = release_year
+        self._likes = 0
+
+    def likes(self):
+        self._likes += 1
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+
+class Movies(Entertainment):
+    def __init__(self, title, release_year, duration):
+        super().__init__(title, release_year)
+        self.duration = duration
+
+    def __str__(self):
+        return f'{self.title} - {self.release_year} - {self.duration} minutos - {self.likes} likes'
+
+
+class TvShow(Entertainment):
+    def __init__(self, title, release_year, seasons):
+        super().__init__(title, release_year)
+        self.seasons = seasons
+
+    def __str__(self):
+        return f'{self.title} - {self.release_year} - {self.seasons} temporadas - {self.likes} likes'
+
+
+class Playlist:
+    def __init__(self, name, shows):
+        self.name = name
+        self.shows = shows
+
+    def size(self):
+        return len(self.shows)
